7a0b45442fe02b1927617383be390bf2e9651dfa
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-11-16 20:10:12 +0530

    Feat: Added Workflow model and integrated TRPC for workflow management with create and fetch functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
-import { requiredAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+"use client";
+import { useTRPC } from "@/trpc/client";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 
-const Page = async () => {
-  await requiredAuth();
-  const data = await caller.getUsers();
-  console.log({ data });
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
+
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+    <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-y-6">
       Protected Server Component
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,8 +2,15 @@ import prisma from "@/lib/db";
 import { protectedProcedure, createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query((opts) => {
-    return prisma.account.findMany({});
+  getWorkflows: protectedProcedure.query((opts) => {
+    return prisma.workflow.findMany({});
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow",
+      },
+    });
   }),
 });
 export type AppRouter = typeof appRouter;
