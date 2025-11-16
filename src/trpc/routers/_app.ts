dbaa1e1b678b27e5866f3cb1ff452a043d0fd3d3
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-11-16 23:22:17 +0530

    Feat: Integrated Inngest for serverless functions and added development scripts for local testing

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,18 @@
 import prisma from "@/lib/db";
 import { protectedProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
   getWorkflows: protectedProcedure.query((opts) => {
     return prisma.workflow.findMany({});
   }),
   createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "rizon.kumar.rahi@gmail.com",
+      },
+    });
     return prisma.workflow.create({
       data: {
         name: "test-workflow",
